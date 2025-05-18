0b88ea1fff56cb2827aef338bfc69610c524e394
toothtaste <197402001+toothtaste@users.noreply.github.com>
2025-05-18 15:24:53 +0700

    fix: webhook err handling

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -9,6 +9,8 @@ export async function POST(req: NextRequest) {
   try {
     const requestJson = await req.json()
 
+    console.log(requestJson)
+
     let data
     try {
       data = await parseWebhookEvent(requestJson, verifyAppKeyWithNeynar)
@@ -17,15 +19,13 @@ export async function POST(req: NextRequest) {
 
       switch (error.name) {
         case "VerifyJsonFarcasterSignature.InvalidDataError":
+          throw new Error("InvalidDataError")
         case "VerifyJsonFarcasterSignature.InvalidEventDataError":
-          // The request data is invalid
-          return Response.json({ success: false, error: error.message }, { status: 400 })
+          throw new Error("InvalidEventDataError")
         case "VerifyJsonFarcasterSignature.InvalidAppKeyError":
-          // The app key is invalid
-          return Response.json({ success: false, error: error.message }, { status: 401 })
+          throw new Error("InvalidAppKeyError")
         case "VerifyJsonFarcasterSignature.VerifyAppKeyError":
-          // Internal error verifying the app key (caller may want to try again)
-          return Response.json({ success: false, error: error.message }, { status: 500 })
+          throw new Error("VerifyAppKeyError")
       }
     }
 
@@ -40,7 +40,7 @@ export async function POST(req: NextRequest) {
           await axios.post("https://api.warpcast.com/v1/frame-notifications", {
             notificationId: randomUUID(),
             title: "Monad Flowers",
-            body: "Frame is now added to your client",
+            body: "Frame is now added",
             targetUrl: "https://monad-flowers.xyz",
             tokens: [event.notificationDetails.token],
           })
@@ -58,7 +58,7 @@ export async function POST(req: NextRequest) {
         await axios.post("https://api.warpcast.com/v1/frame-notifications", {
           notificationId: randomUUID(),
           title: "Monad Flowers",
-          body: "Notifications are now enabled in your client",
+          body: "Notifications are now enabled",
           targetUrl: "https://monad-flowers.xyz",
           tokens: [event.notificationDetails.token],
         })
@@ -71,7 +71,7 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json({ success: true })
   } catch (err) {
-    console.error(err)
+    console.log(err)
     return new NextResponse("Internal Server Error", { status: 500 })
   }
 }
