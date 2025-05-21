26ebeafd61fbc715a5236b25ac46eafd538c4a85
toothtaste <197402001+toothtaste@users.noreply.github.com>
2025-05-21 21:42:48 +0700

    feat: bee order

--- app/frontend/components/Bee.tsx
+++ app/frontend/components/Bee.tsx
@@ -2,29 +2,35 @@ import Image from "next/image"
 import { useState } from "react"
 
 const Bee = () => {
-  const [gif, setGif] = useState<string>()
+  const [gif, setGif] = useState<number>(1)
+  const [visible, setVisible] = useState<boolean>(false)
 
   return (
     <>
       <div
         className="fixed bottom-3/12 animate-bee-flight cursor-pointer z-20"
         onClick={() => {
-          setGif(`/bees/${Math.floor(Math.random() * 7 + 1)}.webp`)
+          setVisible(true)
         }}
       >
         <Image src={"/images/bee.svg"} width={48} height={48} alt="bee" />
       </div>
 
-      {gif && (
+      {visible && (
         <div className="fixed inset-0 z-30">
           <Image
-            src={gif}
+            src={`/bees/${gif}.webp`}
             fill
             unoptimized
             alt="gif"
             className="object-fill object-center"
             onLoad={() => {
-              setTimeout(() => setGif(undefined), 5000)
+              setTimeout(() => {
+                setVisible(false)
+                setGif(prev => {
+                  return prev >= 7 ? 1 : prev + 1
+                })
+              }, 5000)
             }}
           />
         </div>
